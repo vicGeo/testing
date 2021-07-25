983399c8139f6e4ba75dbdb7bc6744717ec8d06a
vicGeo <vicsanig@alumni.upv.es>
2021-07-25 13:30:55 +0200

    add labels form

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,33 @@ import './App.css';
 
 function App() {
   return (
-    <></>
+    <div className='container'>
+      <h1>React with TypeScript</h1>
+      <form>
+        <label htmlFor="userName">Name: </label>
+        <input
+          id="userName"
+          type="text"
+          name="name"
+          value="" />
+
+        <label htmlFor="userAge">Age: </label>
+        <input
+          id="userAge"
+          type="number"
+          name="age"
+          value="" />
+
+        <label htmlFor="userJob">Job: </label>
+        <input
+          id="userJob"
+          type="text"
+          name="job"
+          value="" />
+
+          <button type="submit">Add user: </button>
+      </form>
+    </div>
   );
 }
 
